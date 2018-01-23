85c49944b36e37af4c26cff3fba0aebc604ebd57
Melissa Nock <melnock@gmail.com>
2018-01-23 21:32:58 +0000

    Done.

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -1,7 +1,9 @@
 def run_guessing_game
 
-def help(number)
 
+ loop do 
+puts "Guess a number between 1 and 6."
+number = gets.chomp 
     winning_number = rand(1..6)
   if number == winning_number.to_s
     puts "You guessed the correct number!"
@@ -10,15 +12,6 @@ def help(number)
   else
     puts "The computer guessed #{winning_number}."
  end
-
-end
-
-
-  
- loop do 
-puts "Guess a number between 1 and 6."
-number = gets.chomp 
-help(number)
 break if number == "exit" 
 end
 
